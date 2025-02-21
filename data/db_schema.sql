dba7492beb7977a4bb6d2b98bbd92e0189e5e863
DanushH <danu.nherath@gmail.com>
2025-02-21 07:52:30 +0530

    Modify db schema - Add trigger

--- data/db_schema.sql
+++ data/db_schema.sql
@@ -1,17 +1,51 @@
 CREATE TABLE user (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    username VARCHAR(50) NOT NULL UNIQUE,
-    email VARCHAR(100) NOT NULL UNIQUE,
-    password VARCHAR(255) NOT NULL
+    user_id INT AUTO_INCREMENT PRIMARY KEY,
+    user_name VARCHAR(50) NOT NULL UNIQUE,
+    user_email VARCHAR(100) NOT NULL UNIQUE,
+    user_password VARCHAR(255) NOT NULL
 );
 
 CREATE TABLE prediction (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    date DATE NOT NULL,
-    country VARCHAR(100) NOT NULL,
-    store VARCHAR(100) NOT NULL,
-    product VARCHAR(100) NOT NULL,
-    prediction FLOAT NOT NULL,
-    user_id INT,
-    FOREIGN KEY (user_id) REFERENCES user(id)
+    prediction_id INT AUTO_INCREMENT PRIMARY KEY,
+    prediction_date DATE NOT NULL,
+    prediction_country VARCHAR(100) NOT NULL,
+    prediction_store VARCHAR(100) NOT NULL,
+    prediction_product VARCHAR(100) NOT NULL,
+    prediction_prediction FLOAT NOT NULL,
+    prediction_user_id INT,
+    FOREIGN KEY (prediction_user_id) REFERENCES user(user_id) ON DELETE CASCADE
 );
+
+ALTER TABLE prediction
+	ADD CONSTRAINT unique_prediction 
+		UNIQUE (
+            prediction_user_id, 
+            prediction_date, 
+            prediction_country, 
+            prediction_store, 
+            prediction_product
+        );
+
+
+CREATE TABLE activity (
+    activity_id INT AUTO_INCREMENT PRIMARY KEY,
+    activity_user_id INT NOT NULL,
+    activity_action VARCHAR(255) NOT NULL,
+    activity_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (activity_user_id) REFERENCES user(user_id) ON DELETE CASCADE
+);
+
+DELIMITER $$
+
+CREATE TRIGGER log_user_predictions
+AFTER INSERT ON prediction
+FOR EACH ROW
+BEGIN
+    INSERT INTO activity (activity_user_id, activity_action)
+    VALUES (
+        NEW.prediction_user_id, 
+        CONCAT('Made a prediction with prediction_id: ', NEW.prediction_id)
+    );
+END$$
+
+DELIMITER ;
